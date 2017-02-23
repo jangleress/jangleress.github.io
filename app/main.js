1084c410fc90ef41ba10657e5fce4b89688b783e
jangleress <alyssam@rom.on.ca>
2017-02-23 14:39:57 -0500

    t
    t

--- app/main.js
+++ app/main.js
@@ -24548,7 +24548,17 @@ if (typeof jQuery == "undefined") throw new Error("Bootstrap's JavaScript requir
         googletagmanager("GTM-MMJ5SZ"), e.initialize()
     }), define("main", function() {});
 
+$(document).ready(function() {
+	$('#key .reveal-on-hover').click(function() {
+		$(this).addClass('clicked');
+	});
+	$('#application').click(function() {
+		if ($('#key .reveal-on-hover').hasClass('clicked')) {
+			$(this).hide();
+		}
+	});
+});
+
 $(document).on('click', '#key .reveal-on-hover', function() {
-        $(this).hide();
-		//$(this).toggleClass('popup-key');
+		$(this).toggleClass('popup-key');
  }) ;
