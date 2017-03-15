87147b5e261b780766053d23302b13fd43f491d8
jangleress <alyssam@rom.on.ca>
2017-03-15 09:43:06 -0400

    Revert "Another try"
    This reverts commit 5dbb266f362acea9ed2ff070dad94956ac085505.

--- app/main.js
+++ app/main.js
@@ -23470,10 +23470,7 @@ document.ondragstart = function() {
 };
 
 function refresh() {
-location.href = 'https://romtoronto-map.github.io/#!/layers/home?view=50.384|-59.68|6||1312|697&ocean=atlantic&cat=wildlife';
-	var scale = 'scale(1)';
-	document.style.webkitTransform =  scale;
-	document.style.transform = scale;   
+location.href = 'https://romtoronto-map.github.io/#!/layers/home?view=50.384|-59.68|6||1312|697&ocean=atlantic&cat=wildlife'
 }
 
 function resetTimer() {
